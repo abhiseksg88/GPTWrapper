263779ad9f26115d1a80e6d954da6fa0cf56da01
abhiseksg88 <abhiseksg88@gmail.com>
2025-07-06 17:50:56 -0500

    Add files via upload

---
+++ entities.js
@@ -0,0 +1,41 @@
+import { base44 } from './base44Client';
+
+
+export const Patient = base44.entities.Patient;
+
+export const VitalRecord = base44.entities.VitalRecord;
+
+export const LabResult = base44.entities.LabResult;
+
+export const Prescription = base44.entities.Prescription;
+
+export const AdherenceRecord = base44.entities.AdherenceRecord;
+
+export const SideEffect = base44.entities.SideEffect;
+
+export const ClinicalNote = base44.entities.ClinicalNote;
+
+export const LifestylePlan = base44.entities.LifestylePlan;
+
+export const Goal = base44.entities.Goal;
+
+export const NutritionPlan = base44.entities.NutritionPlan;
+
+export const ExercisePlan = base44.entities.ExercisePlan;
+
+export const ReimbursementAnalysis = base44.entities.ReimbursementAnalysis;
+
+export const Appointment = base44.entities.Appointment;
+
+export const DiagnosticTest = base44.entities.DiagnosticTest;
+
+export const Procedure = base44.entities.Procedure;
+
+export const ICD10Code = base44.entities.ICD10Code;
+
+export const ConditionPlan = base44.entities.ConditionPlan;
+
+
+
+// auth sdk:
+export const User = base44.auth;
